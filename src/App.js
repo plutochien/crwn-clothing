94abced2cca1c491aec997939993956f49c4fa1b
plutochien <pluto.chien@gmail.com>
2019-12-27 19:08:24 +1100

    created project with create create app and add files for homepage'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
+import "./homepage.style.scss";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
