12bced3a8673817433720a191fc8eb9c1083184f
plutochien <pluto.chien@gmail.com>
2019-12-29 14:34:55 +1100

    'update';

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => <div>Hats page</div>;
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
         <Route exact path="/hats" component={HatsPage} />
       </Switch>
     </div>
